cfaafbb73f1478ec20abf55ade6b89dfdaafa625
kingsofcoding <simplecooldude1@gmail.com>
2016-07-27 17:56:34 +0000

    reading time

---
+++ test/build/posts/second/js/scripts.js
@@ -0,0 +1,11 @@
+var elem = document.getElementById("post").textContent;
+
+/* calculate read time */
+var readTime = function( elem ) {
+  var wpm = 200,                      // words per minute the average person reads
+      count = elem.split(' ').length; // get word count from specified element
+  return Math.ceil( count/wpm );      // return number of minutes
+};
+  
+document.getElementById("readTime").innerHTML = readTime(elem);
+
