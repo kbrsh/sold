1b61e04195746b02e6d078276da2a1826cadb0ec
Gopal Shah <simplecooldude1@gmail.com>
2017-04-09 00:14:11 -0700

    init rewrite

--- src/sold.js
+++ src/sold.js
@@ -7,25 +7,19 @@ var Handlebars = require("handlebars");
 var log = require('./util.js').log;
 var error = require("./util.js").error;
 
-Handlebars.registerHelper('===', function(v1, v2, options) {
-  if(v1 === v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
-
 function Sold(dir) {
-    if (!(this instanceof Sold)) return new Sold(dir);
-    log("======= Sold =======", "blue");
-    log("  => intializing", "blue");
-    this.directory = dir;
-    this.posts = [];
-    this.data({});
-    this.source("src");
-    this.postSource("posts");
-    this.destination("build");
-    this.template("template");
-    log("     success\n", "green");
+  if(!(this instanceof Sold)) {
+    return new Sold(dir);
+  }
+  log("======= Sold =======", "blue");
+  this.$directory = dir;
+  this.$data = {};
+  this.$source = "src";
+  this.$destination = "build";
+  this.$template = "template";
+
+
+  return this;
 }
 
 Sold.prototype.configHandlebars = function(config) {
@@ -33,123 +27,79 @@ Sold.prototype.configHandlebars = function(config) {
   return this;
 }
 
-Sold.prototype.postSource = function(location) {
-  this._postSource = path.join(this._source, location);
-  this._postSourcePath = location;
-  return this;
-};
-
-Sold.prototype.source = function(location) {
-  this._source = path.join(this.directory, location);
-  return this;
-};
-
-Sold.prototype.destination = function(location) {
-  this._destination = path.join(this.directory, location);
-  this._postDestination = path.join(this._destination, this._postSourcePath)
-  return this;
-};
-
-Sold.prototype.template = function(location) {
-  this._template = path.join(this.directory, location);
-  this._homeTemplate = path.join(this.directory, location, "index.html");
-  this._postTemplate = path.join(this.directory, location, "post.html");
-  return this;
-};
-
 Sold.prototype.data = function(data) {
-  this._data = data;
+  this.$data = data;
   return this;
 }
 
+Sold.prototype.source = function(dir) {
+  this.$source = dir;
+  return this;
+}
+
+Sold.prototype.destination = function(dir) {
+  this.$destination = dir;
+  return this;
+}
+
+Sold.prototype.template = function(dir) {
+  this.$template = dir;
+  return this;
+}
+
+const compile = function(content, template) {
+  const info = marked(content);
+  const html = info.html;
+  const meta = info.meta;
+
+  return {
+    html: html,
+    meta: meta
+  }
+};
 
 Sold.prototype.build = function() {
-  log("  => building", "blue");
-  this._data.posts = [];
-  var data = this._data;
+  const templatePath = path.join(this.$directory, this.$template);
+  const indexTemplatePath = path.join(templatePath, "index.html");
+  const indexTemplate = fs.readFileSync(indexTemplatePath).toString();
+  const postTemplatePath = path.join(templatePath, "post.html");
+  const postTemplate = fs.readFileSync(postTemplatePath).toString();
 
-  var destination = this._destination;
-  var postDestination = this._postDestination;
+  const sourcePath = path.join(this.$directory, this.$source);
+  const subdirectories = fs.readdirSync(sourcePath);
 
-  var source = this._source;
-  var postSource = this._postSource;
+  const destinationPath = path.join(this.$directory, this.$destination);
 
-  var postTemplate = fs.readFileSync(this._postTemplate).toString();
-  var postTemplatePath = this._postTemplate;
-  var homeTemplate = fs.readFileSync(this._homeTemplate).toString();
-  var homeTemplatePath = this._homeTemplate;
-  var template = this._template;
+  for(var i = 0; i < subdirectories.length; i++) {
+    const subdirectoryName = subdirectories[i];
+    const subdirectorySourcePath = path.join(sourcePath, subdirectoryName);
+    const subdirectoryDestinationPath = path.join(destinationPath, subdirectoryName);
+    mkdirp.sync(subdirectoryDestinationPath);
 
-  // Make destination directory
-  mkdirp(postDestination, (err) => {
-    if(err) {
-      error("Could not create \"" + postDestination + "\" Directory");
-    }
-
-    // Read the source directory
-    var files = fs.readdirSync(postSource);
-
-    for(var i = 0; i < files.length; i++) {
-      var file = files[i];
-
-      // Read file in source directory
-      var content = fs.readFileSync(path.join(postSource, file));
-
-      // Turn destination file to an html file
-      var destinationFile = file.split(".")
-      destinationFile.pop();
-      destinationFile = destinationFile.join(".") + ".html";
-
-      // Compile the data into the post template
-      var postData = postTemplate;
-      var compiled = marked(content.toString());
-      var metadata = compiled.meta;
-      var html = compiled.html;
-      metadata["title"] = metadata.title;
-      metadata["author"] = metadata.author;
-      metadata["description"] = metadata.description;
-      metadata["content"] = html;
-      metadata["file"] = destinationFile;
-      data.posts.push(metadata);
-
-      for(var key in metadata) {
-        metadata["post-" + key] = metadata[key];
-        delete metadata[key];
-      }
-
-      postData = Handlebars.compile(postData)(metadata);
-
-      // Write destination file
-      fs.writeFileSync(path.join(postDestination, destinationFile), postData);
-    }
-
-    // Order posts by `order` metadata
-    data.posts.sort((a, b) => {
-      return a["post-order"] - b["post-order"];
+    const sourceFiles = fs.readdirSync(subdirectorySourcePath);
+    const destinationFiles = sourceFiles.map(function(item) {
+      let trimmed = item.split('.');
+      trimmed.pop();
+      return trimmed.join('.') + '.html';
     });
 
-    // Compile home template
-    var compiledHomeTemplate = homeTemplate;
-    compiledHomeTemplate = Handlebars.compile(compiledHomeTemplate)(data);
+    let allMeta = [];
 
-    // Write home template
-    fs.writeFileSync(path.join(this._destination, "index.html"), compiledHomeTemplate);
+    for(var j = 0; j < sourceFiles.length; j++) {
+      const sourceFilePath = path.join(subdirectorySourcePath, sourceFiles[j]);
+      const destinationFilePath = path.join(subdirectoryDestinationPath, destinationFiles[j]);
 
-    // Copy any other assets in the template to the destination
-    ncp(template, destination, {
-      filter: function(fileName) {
-        return (fileName !== homeTemplatePath) && (fileName !== postTemplatePath);
-      }
-    }, function (err) {
-      if(err) {
-        error("Could not build assets");
-      }
-    });
+      const sourceFileContent = fs.readFileSync(sourceFilePath).toString();
 
-  });
-  log("     success\n", "green");
+      let {html, meta} = compile(sourceFileContent, postTemplate);
+      meta["content"] = html;
+      allMeta.push(meta);
+
+      fs.writeFileSync(destinationFilePath, Handlebars.compile(postTemplate)(meta));
+    }
+  }
+
   return this;
 }
-
 
 module.exports = Sold;
